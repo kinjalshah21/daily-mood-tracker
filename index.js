c7d7af6c16fdd6f1b341f590373218c0193f59f3
Kinjal_Shah <kinjalshah83415@gmail.com>
2025-03-19 00:55:10 +0530

    html and css configured

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// alert('JS file attached!')
+const listMoodItem = document.querySelectorAll(".mood-item");
+const date = document.getElementById("date");
+const time = document.getElementById("time");
+const timeline = document.getElementById("timeline");
+const calender = document.getElementById("calendar");
